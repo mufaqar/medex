e3a2881d0da62372f671f25721890c132dd280b0
syed zunair gillani <gillanix007@gmail.com>
2023-04-26 20:00:16 +0500

    banner update

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -16,7 +16,7 @@ export default class Banner extends Component {
         };
 
         return (
-            <section className='mb-12 overflow-hidden'>
+            <section className='mb-12 mt-20 overflow-hidden'>
                 <div className=''>
                     <Slider {...settings}>
                         <div className='bg-[url(/images/slide-1.jpg)] bg-center bg-no-repeat bg-cover flex items-center min-h-[250px] p-7 sm:pb-24 xl:pb-32 sm:pt-16 xl:pt-24 md:min-h-[400px] xl:min-h-[560px] 2xl:min-h-[750px]'></div>
