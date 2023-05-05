dd636886f33e94f173f03d7a91b5786d42359f68
Mufaqar <mufaqar@gmail.com>
2023-05-05 21:33:33 +0500

    Whatsapp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
-import "@/styles/globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "../config/client";
+import '@/styles/globals.css';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../config/client';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,10 +12,15 @@ export default function App({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} />
         <Footer />
+        <FloatingWhatsApp
+          phoneNumber="00821023208582"
+          accountName="MedexWorlWide"
+          allowEsc
+          allowClickAway
+          notification
+          notificationSound
+        />
       </ApolloProvider>
     </>
   );
 }
-
-
-
