ba4126fe1bd699664ed58b526559ae9846ced8de
Mufaqar <mufaqar@gmail.com>
2023-05-10 11:40:01 +0500

    Blog

---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import React from 'react';
+
+const BlogSingle = () => {
+  return (
+    <h2>Blog Single</h2>
+   
+  );
+};
+
+export default BlogSingle;
---
+++ src/pages/blog/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import React from 'react';
+
+const BlogPosts = () => {
+  return (
+    <h2>Blog Page</h2>
+   
+  );
+};
+
+export default BlogPosts;
