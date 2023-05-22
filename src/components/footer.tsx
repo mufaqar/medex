4e56033028c62b05b17ed86137227f35056a1748
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-05-22 16:33:20 +0500

    footer updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,50 +1,52 @@
 import Link from 'next/link';
 import React from 'react';
 import { HiLocationMarker } from 'react-icons/hi';
-import { BsFacebook, BsFillTelephoneFill, BsInstagram, BsTwitter, BsWhatsapp, BsYoutube } from 'react-icons/bs';
+import { BsFacebook, BsFillTelephoneFill, BsInstagram, BsTwitter, BsWhatsapp, BsYoutube, BsEnvelopeFill } from 'react-icons/bs';
 import { FaFax } from 'react-icons/fa';
-import { BsEnvelopeFill } from 'react-icons/bs';
+import { BiChevronRight } from 'react-icons/bi';
+import { FaCalendarAlt } from 'react-icons/fa';
 import Image from 'next/image';
 import Logo from '../../public/images/logo.png';
 import Img1 from '../../public/images/mri.jpg';
+import Card from '../../public/images/card.png';
 
 export default function Footer() {
     return (
         <footer className=''>
-            <div className='bg-[#540d03] pt-10 md:pt-0'>
+            <div className='bg-[#232B31] pt-10 md:pt-0'>
                 <div className='mx-auto max-w-[1730px] px-4 md:px-6 lg:px-8 2xl:px-20 grid md:grid-cols-3 grid-cols-1 gap-5 md:gap-12 items-center'>
                     <div className='py-10 px-0'>
                         <ul className='flex text-2xl font-medium flex-row space-x-2 md:mt-0'>
                             <li>
-                                <Link href="https://www.facebook.com/medexworlwide/" target="_blank" className='block p-3 rounded-full border border-white/60 hover:border-white text-white/60 hover:text-white '>
+                                <Link href="https://www.facebook.com/medexworlwide/" target="_blank" className='block p-3 rounded-full border border-[#BEBEBE] hover:border-[#9A563A] text-[#BEBEBE] '>
                                     <BsFacebook width={10} />
                                 </Link>
                             </li>
                             <li>
-                                <Link href="https://twitter.com/BotoxKorea" target="_blank" className='block p-3 rounded-full border border-white/60 hover:border-white text-white/60 hover:text-white '>
+                                <Link href="https://twitter.com/BotoxKorea" target="_blank" className='block p-3 rounded-full border border-[#BEBEBE] hover:border-[#9A563A] text-[#BEBEBE] '>
                                     <BsTwitter width={10} />
                                 </Link>
                             </li>
                             <li>
-                                <Link href="https://www.youtube.com/channel/UCSc1ua3BkBlZlUaSvsXgT2Q" target="_blank" className='block p-3 rounded-full border border-white/60 hover:border-white text-white/60 hover:text-white '>
+                                <Link href="https://www.youtube.com/channel/UCSc1ua3BkBlZlUaSvsXgT2Q" target="_blank" className='block p-3 rounded-full border border-[#BEBEBE] hover:border-[#9A563A] text-[#BEBEBE] '>
                                     <BsYoutube width={10} />
                                 </Link>
                             </li>
                             <li>
-                                <Link href="https://www.instagram.com/korea_botox_filler_export" target="_blank" className='block p-3 rounded-full border border-white/60 hover:border-white text-white/60 hover:text-white '>
+                                <Link href="https://www.instagram.com/korea_botox_filler_export" target="_blank" className='block p-3 rounded-full border border-[#BEBEBE] hover:border-[#9A563A] text-[#BEBEBE] '>
                                     <BsInstagram width={10} />
                                 </Link>
                             </li>
                             <li>
-                                <Link href="https://api.whatsapp.com/send?phone=821023208582&text=Get%20Inquiry" target="_blank" className='block p-3 rounded-full border border-white/60 hover:border-white text-white/60 hover:text-white '>
+                                <Link href="https://api.whatsapp.com/send?phone=821023208582&text=Get%20Inquiry" target="_blank" className='block p-3 rounded-full border border-[#BEBEBE] hover:border-[#9A563A] text-[#BEBEBE] '>
                                     <BsWhatsapp width={10} />
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <div className='md:border-x border-x-0 md:border-y-0 border-y border-[#bf18004a] h-full py-10 px-0'>
+                    <div className='md:border-x border-x-0 md:border-y-0 border-y border-[#9A563A94] h-full py-10 px-0'>
                         <Link href="/">
-                            <Image src={Logo} alt="Lono.png" className='w-4/5 m-auto' />
+                            <Image src={Logo} alt="Lono.png" className='w-3/5 m-auto' />
                         </Link>
                     </div>
                     <div className='py-10 px-0'>
@@ -52,12 +54,12 @@ export default function Footer() {
                             OUR NEWSLETTER
                         </h3>
                         <form className="flex items-stretch ">
-                            <input className="bg-gray-100 rounded-lg rounded-r-none text-base leading-none text-gray-800 p-3 w-4/5 border border-transparent focus:outline-none focus:border-none" type="email" placeholder="Your Email" />
-                            <button className="w-32 rounded-l-none hover:bg-[#BF1800] bg-[#BF1800] rounded text-base font-medium leading-none text-white p-3 uppercase focus:outline-none focus:ring-none border border-transparent">subscribe</button>
+                            <input size={40} className="text-sm leading-none text-white placeholder:text-white h-10 p-3 w-4/5 border-b bg-transparent border-[#fde6d84d] focus:outline-none " type="email" placeholder="Enter your email..." />
+                            <button className="w-32 hover:bg-white bg-[#9A563A] duration-200 text-xs tracking-widest leading-none text-[#A3A2A2] hover:text-[#9A563A] font-medium p-3 uppercase focus:outline-none focus:ring-none border border-transparent">subscribe</button>
                         </form>
                     </div>
                 </div>
-                <div className='bg-[#bf18004a] h-[1px]'></div>
+                <div className='bg-[#9A563A94] h-[1px]'></div>
                 <div className='mx-auto max-w-[1730px] px-4 md:px-6 lg:px-8 2xl:px-20 grid md:grid-cols-4 grid-cols-1 gap-5 md:gap-12 py-16 '>
                     <div>
                         <h3 className='mb-4 sm:mb-5 lg:mb-6 pb-0.5 text-lg lg:text-[18px] text-white lg:leading-7 font-medium uppercase'>
@@ -66,29 +68,29 @@ export default function Footer() {
                         <ul className="text-sm lg:text-base flex flex-col space-y-3">
                             <li className="flex items-baseline">
                                 <Link href="https://www.google.com/maps/place/119+Sohyang-ro,+Wonmi-gu,+Bucheon,+Gyeonggi-do,+South+Korea/@37.5025335,126.7525041,15z/data=!3m1!4b1!4m2!3m1!1s0x357b7d3e1121c409:0x3743772ecbe8a8c1" target="_blank"
-                                    className="duration-200 text-white hover:text-[#BF1800] flex ">
-                                    <HiLocationMarker size={14} className="mr-1 mt-1 min-w-[40px]" />
+                                    className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium flex ">
+                                    <HiLocationMarker size={14} className="mr-1 mt-1 min-w-[40px] text-[#9A563A]" />
                                     318 Diart Gallery A Dong , Sohyang Ro 119 , Wonmi-gu,
                                     Bucheon-si Gyeonggi-do , South Korea
                                 </Link>
                             </li>
                             <li className="flex items-baseline">
                                 <Link href="telto:+82-10-2320-8582" target="_blank"
-                                    className="duration-200 text-white hover:text-[#BF1800] flex ">
-                                    <BsFillTelephoneFill size={14} className="mr-1 mt-1 min-w-[40px]" />
+                                    className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium flex ">
+                                    <BsFillTelephoneFill size={14} className="mr-1 mt-1 min-w-[40px] text-[#9A563A]" />
                                     Phone: +82-10-2320-8582
                                 </Link>
                             </li>
                             <li className="flex items-baseline">
-                                <p className="duration-200 text-white hover:text-[#BF1800] flex ">
-                                    <FaFax size={14} className="mr-1 mt-1 min-w-[40px]" />
+                                <p className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium flex ">
+                                    <FaFax size={14} className="mr-1 mt-1 min-w-[40px] text-[#9A563A]" />
                                     Fax: +82-32-3221819
                                 </p>
                             </li>
                             <li className="flex items-baseline">
                                 <Link href="mailto:sonomedex@gmail.com" target="_blank"
-                                    className="duration-200 text-white hover:text-[#BF1800] flex ">
-                                    <BsEnvelopeFill size={14} className="mr-1 mt-1 min-w-[40px]" />
+                                    className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium flex ">
+                                    <BsEnvelopeFill size={14} className="mr-1 mt-1 min-w-[40px] text-[#9A563A]" />
                                     Email: sonomedex@gmail.com
                                 </Link>
                             </li>
@@ -105,23 +107,27 @@ export default function Footer() {
                             IMPORTANT LINKS
                         </h3>
                         <ul className="text-sm lg:text-base flex flex-col space-y-3">
-                            <li className="flex items-baseline">
-                                <Link href="/blog" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/blog" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Blog
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/products" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/products" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Products
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/about-us" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/about-us" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     About us
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/botox-filler" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/botox-filler" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Botox And Filler
                                 </Link>
                             </li>
@@ -132,23 +138,27 @@ export default function Footer() {
                             Categories
                         </h3>
                         <ul className="text-sm lg:text-base flex flex-col space-y-3">
-                            <li className="flex items-baseline">
-                                <Link href="/product-category/botox-and-fillers" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/product-category/botox-and-fillers" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Botox and Fillers
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/product-category/laboratory-instruments" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/product-category/laboratory-instruments" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Laboratory Instruments
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/product-category/medical-equipments" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/product-category/medical-equipments" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Medical Equipments
                                 </Link>
                             </li>
-                            <li className="flex items-baseline">
-                                <Link href="/product-category/ultrasound-scanner" className="duration-200 text-white hover:text-[#BF1800]" >
+                            <li className="flex items-center">
+                                <BiChevronRight size={26} className='text-[#9A563A]' />
+                                <Link href="/product-category/ultrasound-scanner" className="duration-200 text-[#A3A2A2] hover:text-[#9A563A] font-medium" >
                                     Ultrasound Scanner
                                 </Link>
                             </li>
@@ -170,12 +180,12 @@ export default function Footer() {
                                 <div className="md:w-3/4 w-1/2 h-full">
                                     <Link
                                         href="#"
-                                        className="text-lg text-white hover:text-[#BF1800]"
+                                        className="text-lg text-white hover:text-[#9A563A] font-medium"
                                     >
                                         Botox
                                     </Link>
-                                    <p className="text-sm mt-2 text-white">
-                                        16 March 2023
+                                    <p className="text-sm mt-2 text-[#A3A2A2] flex items-center gap-2">
+                                        <FaCalendarAlt size={12} className='text-[#9A563A]' /> 16 March 2023
                                     </p>
                                 </div>
                             </div>
@@ -190,12 +200,12 @@ export default function Footer() {
                                 <div className="md:w-3/4 w-1/2 h-full">
                                     <Link
                                         href="#"
-                                        className="text-lg text-white hover:text-[#BF1800]"
+                                        className="text-lg text-white hover:text-[#9A563A] font-medium"
                                     >
                                         Botox
                                     </Link>
-                                    <p className="text-sm mt-2 text-white">
-                                        16 March 2023
+                                    <p className="text-sm mt-2 text-[#A3A2A2] flex items-center gap-2">
+                                        <FaCalendarAlt size={12} className='text-[#9A563A]' /> 16 March 2023
                                     </p>
                                 </div>
                             </div>
@@ -210,12 +220,12 @@ export default function Footer() {
                                 <div className="md:w-3/4 w-1/2 h-full">
                                     <Link
                                         href="#"
-                                        className="text-lg text-white hover:text-[#BF1800]"
+                                        className="text-lg text-white hover:text-[#9A563A] font-medium"
                                     >
                                         Botox
                                     </Link>
-                                    <p className="text-sm mt-2 text-white">
-                                        16 March 2023
+                                    <p className="text-sm mt-2 text-[#A3A2A2] flex items-center gap-2">
+                                        <FaCalendarAlt size={12} className='text-[#9A563A]' /> 16 March 2023
                                     </p>
                                 </div>
                             </div>
@@ -223,10 +233,17 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className='bg-[#BF1800] p-5'>
-                <p className="text-white text-center text-sm lg:text-base">
-                    © 2010-2023 Medex World Wide. Design & Developed by Mufaqar.com
-                </p>
+            <div className='bg-[#1D2429] p-5 '>
+                <div className='mx-auto max-w-[1730px] px-4 md:px-6 lg:px-8 2xl:px-20 grid md:grid-cols-2 grid-cols-1 gap-5 items-center'>
+                    <div>
+                        <p className="text-white text-sm lg:text-base">
+                            © 2010-2023 Medex World Wide. Design & Developed by Mufaqar.com
+                        </p>
+                    </div>
+                    <div>
+                        <Image src={Card} alt="card.png" className='md:ml-auto ml-0' />
+                    </div>
+                </div>
             </div>
         </footer>
     )
