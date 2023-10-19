9afd993bce379f9682b1f8739f583b82e4fa2efe
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-19 19:13:34 +0500

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,14 +11,8 @@ import { COMPILER_INDEXES } from 'next/dist/shared/lib/constants';
 
 function Header() {
     const [navbar, setNavbar] = useState(false);
-    const [megamenu, setMegamenu] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
-    // const router = useRouter()
-    // const handleRedirect = (path: string) => {
-    //     router.push(path);
-    //     setMegamenu(false)
-    // }
     const [servicesNav, setServicesNav] = useState<any>(null)
+    
     const handleMouseEnter = (idx:number) => {
         setServicesNav(idx)
     }
@@ -32,7 +26,7 @@ function Header() {
             <GoogleAnalytics measurementId="G-TKKQ94RWML" />;
             <header className='fixed top-0 left-0 right-0 bg-white z-40 shadow-md'>
                 <nav className="">
-                    <div onClick={() => { setMegamenu(false), setDropdown(false), setNavbar(false) }} className="hidden md:block bg-dark-gray relative z-50 py-2">
+                    <div onClick={() => { setNavbar(false) }} className="hidden md:block bg-dark-gray relative z-50 py-2">
                         <div className='mx-auto max-w-[1730px] px-4 md:px-6 lg:px-8 2xl:px-20 md:flex flex-wrap justify-between items-center'>
                             <ul className='flex flex-col text-sm font-medium md:flex-row md:space-x-5 md:mt-0'>
                                 <li>
@@ -77,9 +71,9 @@ function Header() {
                     </div>
                     <div className="mx-auto max-w-[1730px] px-4 md:px-6 lg:px-8 2xl:px-20 flex flex-wrap justify-between items-center">
                         <Link href="/" className="flex items-center relative z-50">
-                            <Image src={Logo} alt="logo" className='md:w-full w-3/4' onClick={() => { setMegamenu(false), setDropdown(false), setNavbar(false) }} />
+                            <Image src={Logo} alt="logo" className='md:w-full w-3/4' onClick={() => { setNavbar(false) }} />
                         </Link>
-                        <button onClick={() => { setNavbar(!navbar), setDropdown(false), setMegamenu(false) }} type="button" className="inline-flex items-center p-1.5 my-1 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none  focus:ring-gray-200 relative z-50">
+                        <button onClick={() => { setNavbar(!navbar) }} type="button" className="inline-flex items-center p-1.5 my-1 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none  focus:ring-gray-200 relative z-50">
                             {!navbar ? (
                                 <><span className="sr-only">Open main menu</span><svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg></>
                             ) : (
