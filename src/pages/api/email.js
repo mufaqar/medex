6034833224e20c3bf900380c0a36786553322e11
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-19 14:54:33 +0500

    email integration

---
+++ src/pages/api/email.js
@@ -0,0 +1,43 @@
+export default function (req, res) {
+     // require('dotenv').config()
+     let nodemailer = require('nodemailer')
+
+     const EMAIL="softsgens@gmail.com"
+     const GMAIL_PASSWORD="bczjxzfatgxsebrz"
+
+     // step-1 
+     const transporter = nodemailer.createTransport({
+          port: 465,
+          host: "smtp.gmail.com",
+          auth: {
+               user: EMAIL,
+               pass: GMAIL_PASSWORD
+          },
+          secure: true,
+     })
+
+     const mailData = {
+          from: EMAIL,
+          to: `${req.body.email}`,
+          subject: `Contact Us`,
+          text: `Message From ${req.body.name}`,
+          html: `
+               <p><strong>Full Name: </strong> ${req.body.name}</p>
+               <p><strong>Email: </strong> ${req.body.email}</p>
+               <p><strong>Phone #: </strong> ${req.body.phone}</p>
+               <p><strong>Message: </strong> ${req.body.message}</p>
+          `,
+          
+     }
+
+     // step-3
+     transporter.sendMail(mailData , function (err, info) {
+          if (err)
+               console.log(err)
+          else {
+               res.status(200).json({ message: "email sended!", info })
+          }
+     })
+
+
+}
--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,11 +1,42 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Head from "next/head";
 import {BlogPostsQuery} from '../../config/quries';
 import { client } from '@/config/client';
 import Footer from '../../components/footer';
+import { useForm, SubmitHandler } from "react-hook-form"
+
 
 export default function Contact({blogs}: any) {
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+      } = useForm<any>()
+      const [sending, setSending] = useState<any>()
+
+      const onSubmit: SubmitHandler<any> = (data) => {
+        setSending(true)
+        fetch('/api/email', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          }).then((res) => {
+            console.log('Response received');
+            if (res.status === 200) {
+              console.log('Response succeeded!');
+              alert('Message Successfully send.!');
+              reset();
+              setSending(false);
+            }
+          });
+      }
+      
     return (
         <>
             <Head>
@@ -96,34 +127,32 @@ export default function Contact({blogs}: any) {
                             </div>
                         </div>
                         <div className="w-full md:w-[50%] xl:w-[50%] pb-0.5 lg:pl-12 pt-1.5">
-                            <form className="space-y-5">
+                            <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
                                 <div className="block">
                                     <label htmlFor="name" className="font-normal text-[13px] lg:text-sm leading-none mb-3 cursor-pointer hidden">
                                         Full Name (required)
                                     </label>
-                                    <input type="text" id="name" name="name" placeholder="Enter Your Full Name" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
+                                    <input type="text" {...register("name", { required: true })} id="name" placeholder="Enter Your Full Name" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
                                 </div>
                                 <div className="block">
                                     <label htmlFor="email" className="font-normal text-[13px] lg:text-sm leading-none mb-3 cursor-pointer hidden">
                                         Email Address (required)
                                     </label>
-                                    <input type="email" id="email" name="email" placeholder="Enter Your Email" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
+                                    <input type="email" {...register("email", { required: true })} id="email" placeholder="Enter Your Email" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
                                 </div>
                                 <div className="block">
                                     <label htmlFor="phone" className="font-normal text-[13px] lg:text-sm leading-none mb-3 cursor-pointer hidden">
                                         Phone (Optional)
                                     </label>
-                                    <input type="tel" id="phone" name="phone" placeholder="Enter Your Phone" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
+                                    <input type="tel" id="phone" {...register("phone")} placeholder="Enter Your Phone" className="py-2 px-4 w-full appearance-none border text-input text-[13px] lg:text-sm placeholder-body-color min-h-12 transition duration-200 ease-in-out focus:border-2 focus:outline-none h-11 md:h-12" />
                                 </div>
                                 <div>
                                     <label htmlFor="message" className="font-normal text-[13px] lg:text-sm leading-none mb-3 cursor-pointer hidden">
                                         Message
                                     </label>
-                                    <textarea id="message" name="message" className="px-4 py-3 flex items-center w-full appearance-none transition duration-300 ease-in-out  text-[13px] lg:text-sm focus:outline-none focus:ring-0 placeholder-body-color border focus:bg-white focus:border-2" rows={4} placeholder="Briefly describe.."></textarea>
+                                    <textarea id="message" {...register("message", { required: true })} name="message" className="px-4 py-3 flex items-center w-full appearance-none transition duration-300 ease-in-out  text-[13px] lg:text-sm focus:outline-none focus:ring-0 placeholder-body-color border focus:bg-white focus:border-2" rows={4} placeholder="Briefly describe.."></textarea>
                                 </div>
-                                <button type="submit" className="btn text-sm lg:text-[15px] leading-4 inline-flex items-center transition ease-in-out duration-300 font-semibold text-center justify-center tracking-[0.2px] h-11 md:h-[50px] bg-Brown  text-white hover:text-white hover:bg-title-color px-5 lg:px-6 py-4 md:py-3.5 lg:py-4 w-full">
-                                    Send Message
-                                </button>
+                                <input type="submit" value={sending ? 'Sending...' : 'Send Message'} className="btn text-sm cursor-pointer lg:text-[15px] leading-4 inline-flex items-center transition ease-in-out duration-300 font-semibold text-center justify-center tracking-[0.2px] h-11 md:h-[50px] bg-Brown  text-white hover:text-white hover:bg-title-color px-5 lg:px-6 py-4 md:py-3.5 lg:py-4 w-full" />
                             </form>
                         </div>
                     </div>
